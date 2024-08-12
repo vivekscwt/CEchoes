55c4326fc398f3061c5c58455daf10d7a40647c2
Sudipta <dev2.scwt@gmail.com>
2024-08-12 11:37:36 +0530

    complaint changes of excel sheet

--- controllers/auth.js
+++ controllers/auth.js
@@ -12825,13 +12825,108 @@ exports.editDiscussions = async (req, res) => {
 
                 if (userResults.length > 0) {
                     const { email: userEmail, first_name: firstName, last_name: lastName } = userResults[0];
-                    const fullName = `${firstName} ${lastName}`;
+                    var fullName = `${firstName} ${lastName}`;
 
                     const mailOptions = {
                         from: process.env.MAIL_USER,
                         to: userEmail,
                         subject: 'Comment approval mail',
-                        html: `<p>Hello ${fullName},</p><p>Your comment has been approved.</p>`
+                        html: `<div id="wrapper" dir="ltr" style="background-color: #f5f5f5; margin: 0; padding: 70px 0 70px 0; -webkit-text-size-adjust: none !important; width: 100%;">
+                                <style>
+                                body, table, td, p, a, h1, h2, h3, h4, h5, h6, div {
+                                    font-family: Calibri, 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif !important;
+                                }
+                                </style>
+                                <table height="100%" border="0" cellpadding="0" cellspacing="0" width="100%">
+                                 <tbody>
+                                  <tr>
+                                   <td align="center" valign="top">
+                                     <div id="template_header_image"><p style="margin-top: 0;"></p></div>
+                                     <table id="template_container" style="box-shadow: 0 1px 4px rgba(0,0,0,0.1) !important; background-color: #fdfdfd; border: 1px solid #dcdcdc; border-radius: 3px !important;" border="0" cellpadding="0" cellspacing="0" width="600">
+                                      <tbody>
+                                        <tr>
+                                         <td align="center" valign="top">
+                                           <!-- Header -->
+                                           <table id="template_header" style="background-color: #000; border-radius: 3px 3px 0 0 !important; color: #ffffff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: &quot;Helvetica Neue&quot;, Helvetica, Roboto, Arial, sans-serif;" border="0" cellpadding="0" cellspacing="0" width="600">
+                                             <tbody>
+                                               <tr>
+                                               <td><img alt="Logo" src="${process.env.MAIN_URL}front-end/images/cechoes-logo.png"  style="padding: 30px 40px; display: block;  width: 70px;" /></td>
+                                                <td id="header_wrapper" style="padding: 36px 48px; display: block;">
+                                                   <h1 style="color: #FCCB06; font-family: &quot;Helvetica Neue&quot;, Helvetica, Roboto, Arial, sans-serif; font-size: 30px; font-weight: bold; line-height: 150%; margin: 0; text-align: left;"> Comment on ${content} has been approved.</h1>
+                                                </td>
+                          
+                                               </tr>
+                                             </tbody>
+                                           </table>
+                                     <!-- End Header -->
+                                     </td>
+                                        </tr>
+                                        <tr>
+                                         <td align="center" valign="top">
+                                           <!-- Body -->
+                                           <table id="template_body" border="0" cellpadding="0" cellspacing="0" width="600">
+                                             <tbody>
+                                               <tr>
+                                                <td id="body_content" style="background-color: #fdfdfd;" valign="top">
+                                                  <!-- Content -->
+                                                  <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                                   <tbody>
+                                                    <tr>
+                                                     <td style="padding: 48px;" valign="top">
+                                                       <div id="body_content_inner" style="color: #737373; font-family: &quot;Helvetica Neue&quot;, Helvetica, Roboto, Arial, sans-serif; font-size: 14px; line-height: 150%; text-align: left;">
+                                                        
+                                                        <table border="0" cellpadding="4" cellspacing="0" width="90%">
+                                                          <tr>
+                                                            <td colspan="2">
+                                                            <strong>Hello ${full_name},</strong>
+                                                            <p style="font-size:15px; line-height:20px">Your comment for the discussion has been approved. Now you can see your comment on the <a href="${process.env.MAIN_URL}discussion-details/${discussion_id}">here</a>.</p>
+                                                            </td>
+                                                          </tr>
+                                                        </table>
+                                                        
+                                                       </div>
+                                                     </td>
+                                                    </tr>
+                                                   </tbody>
+                                                  </table>
+                                                <!-- End Content -->
+                                                </td>
+                                               </tr>
+                                             </tbody>
+                                           </table>
+                                         <!-- End Body -->
+                                         </td>
+                                        </tr>
+                                        <tr>
+                                         <td align="center" valign="top">
+                                           <!-- Footer -->
+                                           <table id="template_footer" border="0" cellpadding="10" cellspacing="0" width="600">
+                                            <tbody>
+                                             <tr>
+                                              <td style="padding: 0; -webkit-border-radius: 6px;" valign="top">
+                                               <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                                                 <tbody>
+                                                   <tr>
+                                                    <td colspan="2" id="credit" style="padding: 20px 10px 20px 10px; -webkit-border-radius: 0px; border: 0; color: #fff; font-family: Arial; font-size: 12px; line-height: 125%; text-align: center; background:#000" valign="middle">
+                                                         <p>This email was sent from <a style="color:#FCCB06" href="${process.env.MAIN_URL}">CEchoesTechnology</a></p>
+                                                    </td>
+                                                   </tr>
+                                                 </tbody>
+                                               </table>
+                                              </td>
+                                             </tr>
+                                            </tbody>
+                                           </table>
+                                         <!-- End Footer -->
+                                         </td>
+                                        </tr>
+                                      </tbody>
+                                     </table>
+                                   </td>
+                                  </tr>
+                                 </tbody>
+                                </table>
+                               </div>`
                     };
 
                     await mdlconfig.transporter.sendMail(mailOptions);
@@ -12854,7 +12949,103 @@ exports.editDiscussions = async (req, res) => {
                         from: process.env.MAIL_USER,
                         to: authorEmail,
                         subject: 'Comment Approval',
-                        html: `<p>Hello ${authorFullName},</p><p>Your discussion has received an approved comment.</p>`
+                        html: 'Comment Approval',
+                                html: `<div id="wrapper" dir="ltr" style="background-color: #f5f5f5; margin: 0; padding: 70px 0 70px 0; -webkit-text-size-adjust: none !important; width: 100%;">
+                                <style>
+                                body, table, td, p, a, h1, h2, h3, h4, h5, h6, div {
+                                    font-family: Calibri, 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif !important;
+                                }
+                                </style>
+                                <table height="100%" border="0" cellpadding="0" cellspacing="0" width="100%">
+                                 <tbody>
+                                  <tr>
+                                   <td align="center" valign="top">
+                                     <div id="template_header_image"><p style="margin-top: 0;"></p></div>
+                                     <table id="template_container" style="box-shadow: 0 1px 4px rgba(0,0,0,0.1) !important; background-color: #fdfdfd; border: 1px solid #dcdcdc; border-radius: 3px !important;" border="0" cellpadding="0" cellspacing="0" width="600">
+                                      <tbody>
+                                        <tr>
+                                         <td align="center" valign="top">
+                                           <!-- Header -->
+                                           <table id="template_header" style="background-color: #000; border-radius: 3px 3px 0 0 !important; color: #ffffff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: &quot;Helvetica Neue&quot;, Helvetica, Roboto, Arial, sans-serif;" border="0" cellpadding="0" cellspacing="0" width="600">
+                                             <tbody>
+                                               <tr>
+                                               <td><img alt="Logo" src="${process.env.MAIN_URL}front-end/images/cechoes-logo.png"  style="padding: 30px 40px; display: block;  width: 70px;" /></td>
+                                                <td id="header_wrapper" style="padding: 36px 48px; display: block;">
+                                                   <h1 style="color: #FCCB06; font-family: &quot;Helvetica Neue&quot;, Helvetica, Roboto, Arial, sans-serif; font-size: 30px; font-weight: bold; line-height: 150%; margin: 0; text-align: left;">Comment on ${content} has been approved.</h1>
+                                                </td>
+                          
+                                               </tr>
+                                             </tbody>
+                                           </table>
+                                     <!-- End Header -->
+                                     </td>
+                                        </tr>
+                                        <tr>
+                                         <td align="center" valign="top">
+                                           <!-- Body -->
+                                           <table id="template_body" border="0" cellpadding="0" cellspacing="0" width="600">
+                                             <tbody>
+                                               <tr>
+                                                <td id="body_content" style="background-color: #fdfdfd;" valign="top">
+                                                  <!-- Content -->
+                                                  <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                                   <tbody>
+                                                    <tr>
+                                                     <td style="padding: 48px;" valign="top">
+                                                       <div id="body_content_inner" style="color: #737373; font-family: &quot;Helvetica Neue&quot;, Helvetica, Roboto, Arial, sans-serif; font-size: 14px; line-height: 150%; text-align: left;">
+                                                        
+                                                        <table border="0" cellpadding="4" cellspacing="0" width="90%">
+                                                          <tr>
+                                                            <td colspan="2">
+                                                            <strong>Hello ${full_name},</strong>
+                                                            <p style="font-size:15px; line-height:20px">Comment for the ${content} discussion has been approved. Now you can see the discusion comment on the <a href="${process.env.MAIN_URL}discussion-details/${discussion_id}">here</a>.</p>
+                                                            </td>
+                                                          </tr>
+                                                        </table>
+                                                        
+                                                       </div>
+                                                     </td>
+                                                    </tr>
+                                                   </tbody>
+                                                  </table>
+                                                <!-- End Content -->
+                                                </td>
+                                               </tr>
+                                             </tbody>
+                                           </table>
+                                         <!-- End Body -->
+                                         </td>
+                                        </tr>
+                                        <tr>
+                                         <td align="center" valign="top">
+                                           <!-- Footer -->
+                                           <table id="template_footer" border="0" cellpadding="10" cellspacing="0" width="600">
+                                            <tbody>
+                                             <tr>
+                                              <td style="padding: 0; -webkit-border-radius: 6px;" valign="top">
+                                               <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                                                 <tbody>
+                                                   <tr>
+                                                    <td colspan="2" id="credit" style="padding: 20px 10px 20px 10px; -webkit-border-radius: 0px; border: 0; color: #fff; font-family: Arial; font-size: 12px; line-height: 125%; text-align: center; background:#000" valign="middle">
+                                                         <p>This email was sent from <a style="color:#FCCB06" href="${process.env.MAIN_URL}">CEchoesTechnology</a></p>
+                                                    </td>
+                                                   </tr>
+                                                 </tbody>
+                                               </table>
+                                              </td>
+                                             </tr>
+                                            </tbody>
+                                           </table>
+                                         <!-- End Footer -->
+                                         </td>
+                                        </tr>
+                                      </tbody>
+                                     </table>
+                                   </td>
+                                  </tr>
+                                 </tbody>
+                                </table>
+                               </div>`
                     };
 
                     await mdlconfig.transporter.sendMail(authorMailOptions);
